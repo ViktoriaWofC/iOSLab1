13f1445936e638723fd12d8b50c927d4cd814c1a
user <user@mini-3.local>
2017-03-18 09:53:58 +0400

    add alert

--- Lab_1/ViewController.h
+++ Lab_1/ViewController.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-
-
+- (IBAction)update:(id)sender;
+@property (copy, nonatomic) NSString *testString;
 @end
 
--- Lab_1/ViewController.m
+++ Lab_1/ViewController.m
@@ -9,14 +9,31 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *testLabel;
 
 @end
 
 @implementation ViewController
-
+@synthesize testString = _testString;
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+}
+- (IBAction)update:(id)sender {
+    self.testString = @"32raw4q 535q34";
+    self.testLabel.text = self.testString;
+    
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Simple" message:@"Simple alertView demo with Cancel and OK." preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction * action) {
+        NSLog(@"Cancel");
+    }];
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {
+        NSLog(@"OK");
+    }];
+    
+    [alertController addAction:cancelAction];
+    [alertController addAction:okAction];
+    [self presentViewController:alertController animated: YES completion: nil];
 }
 
 
