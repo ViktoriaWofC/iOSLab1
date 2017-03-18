ffd65d4f822053f0374ae9acc07aea33accf485a
user <user@mini-3.dlink>
2017-03-18 08:33:55 +0400

    Initial Commit

---
+++ Lab_1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lab_1
+//
+//  Created by user on 18.03.17.
+//  Copyright © 2017 edu.self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lab_1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lab_1
+//
+//  Created by user on 18.03.17.
+//  Copyright © 2017 edu.self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
