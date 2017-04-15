18bec2f4adefdb38d8738adbb66df25238f62767
user <user@mini-3.dlink>
2017-04-15 10:41:08 +0400

    zap

--- Lab_1/ViewController.m
+++ Lab_1/ViewController.m
@@ -17,10 +17,12 @@
 @implementation ViewController
 @synthesize testString = _testString;
 @synthesize testArray = _testArray;
-UIActivityIndicatorView *indicator;
+UIActivityIndicatorView *progress;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [self createProgress];
     // Do any additional setup after loading the view, typically from a nib.
     self.testArray = [[NSMutableArray alloc] init];
     //[self.testArray addObject:[NSString stringWithFormat:@"element #%d",80]];
@@ -36,34 +38,49 @@ UIActivityIndicatorView *indicator;
     [self.buttonUpdate setTitle:update forState:UIControlStateNormal];
     //[self.buttonUpdate setTitle: strTabSearch];
     //[self.findButton setTitle: loc forState: UIControlStateNormal];
+    
+    
+    //[self sendRequest];
+    
 }
 
 - (IBAction)update:(id)sender {
-    self.testString = @"32raw4q 535q34";
-    self.testLabel.text = self.testString;
+    //self.testString = @"32raw4q 535q34";
+    //self.testLabel.text = self.testString;
     
-    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Simple" message:@"Simple alertView demo with Cancel and OK." preferredStyle:UIAlertControllerStyleAlert];
-    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction * action) {
-        NSLog(@"Cancel");
-    }];
-    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {
-        NSLog(@"OK");
-    }];
+    //UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Simple" message:@"Simple alertView demo with Cancel and OK." preferredStyle:UIAlertControllerStyleAlert];
+    //UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction * action) {
+     //   NSLog(@"Cancel");
+    //}];
+    //UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {
+    //    NSLog(@"OK");
+    //}];
     
-    [alertController addAction:cancelAction];
-    [alertController addAction:okAction];
-    [self presentViewController:alertController animated: YES completion: nil];
+    //[alertController addAction:cancelAction];
+    //[alertController addAction:okAction];
+    //[self presentViewController:alertController animated: YES completion: nil];
 
     //[self updateTable];
+    
+    //[progress startAnimating];
     [self sendRequest];
 }
 
-
+- (void) createProgress {
+    progress = [[UIActivityIndicatorView alloc]initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    progress.frame = CGRectMake(0.0, 0.0, 60.0, 60.0);
+    progress.center = self.view.center;
+    [self.view addSubview:progress];
+    [progress bringSubviewToFront:self.view];
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = TRUE;
+}
 //123
 - (void) sendRequest{
-    //[indicator startAnimating];
+    [progress startAnimating];
+    
     
     //NSString *serverAddress = @"https://tosamara.ru/xml_bridge.php";
+    //NSURL *url = [NSURL URLWithString:@"https://tosamfdghfdhara.ru/xml_bridge.php"];
     NSURL *url = [NSURL URLWithString:@"https://tosamara.ru/xml_bridge.php"];
     //@"http://kparser.pp.ua/json/search/"
     //https://tosamara.ru/spravka/ostanovki/9
@@ -82,10 +99,26 @@ UIActivityIndicatorView *indicator;
         NSString *jsonResp =[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
         self.json = jsonResp;
         NSLog(@"%@",jsonResp);
-        Parser *parser = [[Parser alloc] init];
+        //Parser *parser = [[Parser alloc] init];
         //tableData = [parser getMoviesArray:jsonCiril];
         //tableDataID = [parser getMoviesID:jsonCiril];
-        NSString *servetyutyus = @"https:/fghfgh vki/9";
+        //NSString *servetyutyus = @"https:/fghfgh vki/9";
+        
+        [self parse];
+        
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            //[self updateTable];
+            [progress stopAnimating];
+            NSLog(@"2: %@", error);
+            if(error!=nil){
+                UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Внимание" message:@"Ошибка подключения!" delegate:self cancelButtonTitle:@"Попробовать еще раз" otherButtonTitles:@"Отмена", nil];
+                [alert show];            }
+            else {
+            [self updateTable];
+            }
+        });
+        
     }];
     
    /* NSURLSessionDataTask *getJsonTask = [session dataTaskWithURL:[NSURL URLWithString:serverAddress] completionHandler:^(NSData *data, NSURLResponse *response, NSError *error){
@@ -102,12 +135,32 @@ UIActivityIndicatorView *indicator;
 
     [getJsonTask resume];
     
-    id item   =[NSString stringWithFormat:@"element #%d",9];
-    [self.testArray removeAllObjects];
-    [self.testArray addObject:item];
+    //id item   =[NSString stringWithFormat:@"element #%d",9];
+    //[self.testArray removeAllObjects];
+    //[self.testArray addObject:item];
     
-    [self updateTable];
+    //[self updateTable];
 
+}
+
+-(void) parse{
+    
+    NSString *pattern =@"\\b(trans-bus)|(trans-trol)\\b([a-z0-9<>])\\b(</td>)\\b";
+    NSError *err = NULL;
+    
+    NSRegularExpressionOptions regularOptions = NSRegularExpressionCaseInsensitive;
+    NSRegularExpression *regular  = [NSRegularExpression regularExpressionWithPattern:pattern options:regularOptions error:&err];
+    
+    if(err){
+        NSLog(@"Err");
+    }
+    
+    NSInteger count = [regular numberOfMatchesInString:self.json options:0 range:NSMakeRange(0,[self.json length])];
+    NSRange range = [regular rangeOfFirstMatchInString:self.json options:0 range:NSMakeRange(0,[self.json length])];
+    if(!NSEqualRanges(range, NSMakeRange(NSNotFound, 0))){
+        NSString *str = [self.json substringWithRange:range];
+        NSLog(@"%@",str);
+    }
 }
 
 
